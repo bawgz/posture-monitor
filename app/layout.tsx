b1616333c24de0022f993b5627bf42b2683bd8a6
Luke Boggs <lukeboggs7@gmail.com>
2023-12-03 21:12:27 -0600

    Add a navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
+import logo from './favicon.ico'; // with import
+
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -15,7 +17,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <nav className="border-gray-200 bg-black dark:bg-black dark:border-black">
+          <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+            <span className="flex items-center space-x-3 rtl:space-x-reverse">
+              <img src={logo.src} className="h-8" alt="Flowbite Logo" />
+              <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Posture Monitor</span>
+            </span>
+          </div>
+        </nav>
+        {children}
+      </body>
     </html>
   );
 }
