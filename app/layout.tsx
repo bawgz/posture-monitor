b24d6b09850c910aba26b77ca5595a887cf1bb60
Luke Boggs <lukeboggs7@gmail.com>
2023-12-12 16:53:38 -0600

    Add Speed Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 import { Inter } from 'next/font/google';
 import logo from './favicon.ico';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -28,6 +29,7 @@ export default function RootLayout({
         </nav>
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
